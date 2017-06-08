1d5150595b8c417371a59c6634fc1cbf2afbcb29
Victor Boschuk <nakedsnake.boschuk@gmail.com>
2017-06-09 00:16:54 +0300

    -initial mock project added

---
+++ HebbNeuron/HebbNeuron/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HebbNeuron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
