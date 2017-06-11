e2324917b4579c3ffc288a77e1369330c178abf8
Victor Boschuk <nakedsnake.boschuk@gmail.com>
2017-06-11 16:52:16 +0300

    -RosenblattNeuron with demo

--- NeuralNetworks/ConsoleDemoCore/Program.cs
+++ NeuralNetworks/ConsoleDemoCore/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleDemoCore
         {
             Console.WriteLine("Hello NN World!");
 
+            TestRosenblattNeuron();
+        }
+
+        static void TestHebbNeuron()
+        {
             var sample = new List<List<int>>
             {
                 new List<int> {-1,-1 },
@@ -18,13 +23,7 @@ namespace ConsoleDemoCore
                 new List<int> { 1, 1 },
             };
 
-            var targets = new List<int>
-            {
-                -1,
-                1,
-                1,
-                1
-            };
+            var targets = new List<int> { -1, 1, 1, 1 };
 
             var neuron = new HebbNeuron();
 
@@ -34,6 +33,32 @@ namespace ConsoleDemoCore
             neuron.Train(sample, targets);
             Console.WriteLine("test 2");
             neuron.Test(sample);
+        }
+
+        static void TestRosenblattNeuron()
+        {
+            var samples = new List<List<double>>
+            {
+                new List<double> {-1,-1 },
+                new List<double> {-1, 1 },
+                new List<double> { 1,-1 },
+                new List<double> { 1, 1 },
+            };
+
+            var targets = new List<double>
+            {
+                -1, -1, -1, 1
+            };
+
+            var neuron = new RosenblattNeuron(0.1d);
+            neuron.Test(samples);
+
+            var epochsCount = neuron.Train(samples, targets);
+            Console.WriteLine(string.Format("Epoch count = {0}",epochsCount));
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("After learn");
+            neuron.Test(samples);
         }
     }
 }
--- NeuralNetworks/Neurons/RosenblattNeuron.cs
+++ NeuralNetworks/Neurons/RosenblattNeuron.cs
@@ -1,8 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Neurons
 {
+    // Logical and
+    // x1   x2   AND
+    // -1   -1   -1
+    // -1    1   -1
+    //  1   -1   -1
+    //  1    1    1
+
     public class RosenblattNeuron
     {
+        public double Weight1 { get; set; }
+        public double Weight2 { get; set; }
+        public double Threshold { get; set; }
+
+        public double Rate { get; set; }
+
+
+        public RosenblattNeuron(double rate)
+        {
+            Weight1 = new Random().NextDouble();
+            Weight2 = new Random().NextDouble();
+            Threshold = new Random().NextDouble();
+            Rate = rate;
+        }
+
+        public double Activate(List<double> sample)
+        {
+            var weightedSum = Weight1 * sample[0] +
+                              Weight2 * sample[1] +
+                              Threshold;
+
+            var y = ThresActivateFunction(weightedSum);
+            return y;
+        }
+
+        public double ThresActivateFunction(double x)
+        {
+            return x < 0 ? -1 : 1;
+        }
+
+        public void Test(List<List<double>> samples)
+        {
+            foreach (var sample in samples)
+            {
+                var weightedSum = Weight1 * sample[0] +
+                                  Weight2 * sample[1] +
+                                  Threshold;
+                var y = ThresActivateFunction(weightedSum);
+
+                Console.WriteLine(string.Format("({0}, {1}): {2}", sample[0], sample[1], y));
+
+            }
+        }
+
+        public int Train(List<List<double>> samples, List<double> targets)
+        {
+            var isFinish = false;
+
+            var epochsCount = 0;
+
+            while (!isFinish)
+            {
+                isFinish = true;
+                for (var index = 0; index < samples.Count; index++)
+                {
+                    
+                    var y = Activate(samples[index]);
+                    // TODO fix with tolerance
+                    if (y != targets[index])
+                    {
+                        isFinish = false;
+                        Weight1 += Rate * samples[index][0] * targets[index];
+                        Weight2 += Rate * samples[index][1] * targets[index];
+                        Threshold += Rate * targets[index];
+                    }
+                }
+                epochsCount++;
+            }
+
+            return epochsCount;
+        }
     }
 }
